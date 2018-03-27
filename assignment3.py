09908168f8800237e8faee35e2aba10d809d4fb6
hejavad <he.javad@yahoo.com>
2018-03-27 19:09:30 +0430

    add some lines code and add file host_name

--- assignment3.py
+++ assignment3.py
@@ -1,14 +1,20 @@
 import socket
 
 ports=[21,22,25,80,110,9090]
-socket.setdefaulttimeout(5)
+socket.setdefaulttimeout(10)
 f=open('host_names','r')
 host_names=f.read()
 host_names=host_names.split('\n')
 
 for x in host_names:
+    print(x)
     for y in ports:
+        print(y)
         try:
-           # you should write your codes in this block
+            s = socket.socket()
+            s.connect((x, y))
+            ans = s.recv(1024)
+            s.close()
+            print(ans)
         except Exception as e:
             print(e)
