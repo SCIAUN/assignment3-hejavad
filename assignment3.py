c89154257af5c622e5019617298ddb985ee3010c
hejavad <he.javad@yahoo.com>
2018-03-27 22:59:02 +0430

    add garab banner

--- assignment3.py
+++ assignment3.py
@@ -1,7 +1,7 @@
 import socket
 
 ports=[21,22,25,80,110,9090]
-socket.setdefaulttimeout(10)
+socket.setdefaulttimeout(5)
 f=open('host_names','r')
 host_names=f.read()
 host_names=host_names.split('\n')
@@ -16,5 +16,15 @@ for x in host_names:
             ans = s.recv(1024)
             s.close()
             print(ans)
+            if "FreeFloat Ftp Server (Version 1.00)" in ans:
+                print("[+] FreeFloat server is vulnerable ", '\n');
+            elif "3Com 3CDaemon FTP Server Version 2.0" in ans:
+                print("[+] 3CDaemon server is vulnerable ", '\n');
+            elif "Ability Server 2.34" in ans:
+                print("[+] Ability server is vulnerable ", '\n');
+            elif "Sami FTP Server 2.0.2" in ans:
+                print("[+] Sami server is vulnerable ", '\n');
+            else:
+                print("[-] FTP server is not vulnerable", '\n');
         except Exception as e:
             print(e)
